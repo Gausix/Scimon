c68b693f00d4106c0d305d4c2800d807b93d4a96
Kremilly <contact@kremilly.com>
2025-02-27 12:31:45 -0300

    feat: update API endpoint URLs to use new domain for Scimon services

--- src/consts/addons.rs
+++ src/consts/addons.rs
@@ -8,8 +8,8 @@ impl Addons {
     // Scimon
     pub const MONLIB_API_REQUEST: &'static str = "http://localhost/";
     pub const SCIMON_SCRAPE_API_ENDPOINT: &'static str = "https://addons.scimon.dev/pdfscrape?url=";
-    pub const SCIMON_URLFILTER_API_ENDPOINT: &'static str = "https://addons.scibun.com/urlfilter?url=";
+    pub const SCIMON_URLFILTER_API_ENDPOINT: &'static str = "https://addons.scimon.dev/urlfilter?url=";
 
-    pub const README_TEMPLATE_LINK: &'static str = "https://template.scibun.com/";
+    pub const README_TEMPLATE_LINK: &'static str = "https://readme.scimon.dev/";
     
 }
