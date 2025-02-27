954672ef7ad0702929832030f0f154daa1e27f84
Kremilly <contact@kremilly.com>
2025-02-27 12:28:25 -0300

    feat: update default CSS style and scrape API endpoint URIs to use new domain

--- src/consts/addons.rs
+++ src/consts/addons.rs
@@ -3,11 +3,11 @@ pub struct Addons;
 impl Addons {
 
     pub const DOWNLOAD_FILES_URI: &'static str = "https://raw.githubusercontent.com/Gausix/Scimon/main/";
-    pub const DEFAULT_CSS_STYLE: &'static str = "https://addons.scibun.com/static/md-default.css";
+    pub const DEFAULT_CSS_STYLE: &'static str = "https://addons.scimon.dev/static/md-default.css";
 
     // Scimon
     pub const MONLIB_API_REQUEST: &'static str = "http://localhost/";
-    pub const SCIMON_SCRAPE_API_ENDPOINT: &'static str = "https://addons.scibun.com/scrape?url=";
+    pub const SCIMON_SCRAPE_API_ENDPOINT: &'static str = "https://addons.scimon.dev/pdfscrape?url=";
     pub const SCIMON_URLFILTER_API_ENDPOINT: &'static str = "https://addons.scibun.com/urlfilter?url=";
 
     pub const README_TEMPLATE_LINK: &'static str = "https://template.scibun.com/";
