f770116eab3ab2a14c3dec9809bb368631750524
Kremilly <contact@kremilly.com>
2025-03-16 03:14:14 -0300

    fix: update logout function to use constant for MONLIB_API_ENV

--- src/monlib/logout.rs
+++ src/monlib/logout.rs
@@ -1,4 +1,5 @@
 use crate::{
+    consts::addons::Addons,
     configs::write_env::WriteEnv,
     ui::errors_alerts::ErrorsAlerts,
 };
@@ -8,7 +9,7 @@ pub struct MonlibLogout;
 impl MonlibLogout {
 
     pub fn logout(&self) {
-        if let Err(err) = WriteEnv::new(Some("MONLIB_API_KEY".to_owned()), Some("".to_string())).edit_env_var() {
+        if let Err(err) = WriteEnv::new(Some(Addons::MONLIB_API_ENV.to_owned()), Some("".to_string())).edit_env_var() {
             ErrorsAlerts::generic(&err.to_string());
         }
     }
