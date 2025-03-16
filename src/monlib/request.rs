5ab996ee3bd2605ff89bfba9aa397aaf915d57c6
Kremilly <contact@kremilly.com>
2025-03-16 06:05:28 -0300

    fix: update request method to use a more specific error type for improved error handling

--- src/monlib/request.rs
+++ src/monlib/request.rs
@@ -1,6 +1,7 @@
 extern crate reqwest;
 
 use reqwest::{
+    Error,
     Client, 
     Response
 };
@@ -14,10 +15,10 @@ pub struct MonlibRequest;
 
 impl MonlibRequest {
 
-    pub async fn request(&self, url: &str) -> Result<Response, reqwest::Error> {
+    pub async fn request(&self, url: &str) -> Result<Response, Error> {
         let api_key = Env.env_var(Addons::MONLIB_API_ENV); 
         let client = Client::builder().danger_accept_invalid_certs(true).build().unwrap();
-        
+
         let response = client
             .get(url)
             .header("API-Key", api_key)
